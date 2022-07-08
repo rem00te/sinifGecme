973df8a8b87969f0e4327fec5de0f7d2f8fd46a6
Onur ALTINTOP <rem00te@hotmail.com>
2022-07-08 16:43:26 +0300

    Initial commit

---
+++ src/sinif.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class sinif {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int sinif=0;
+        int mat = 0, fiz = 0, turk = 0, kim = 0, muz = 0, ortalama;
+        System.out.print("Matematik Ders Notunu Girin : ");
+        mat = input.nextInt();
+
+        if (mat<0 || mat>=101) {
+            System.out.println("Matematik notu için 0-100 arasında bir not değeri giriniz.");
+
+        }
+
+        else if(mat>0 && mat< 101)  {
+            System.out.print("Fizik Ders Notunu Girin : ");
+            fiz = input.nextInt();
+
+        }
+
+        if (fiz<0 || fiz>= 101) {
+            System.out.println("Fizik notu için 0-100 arasında bir not değeri giriniz.");
+        }
+
+        else if (fiz>0 && fiz< 101){
+            System.out.print("Türkçe Ders Notunu Girin : ");
+            turk = input.nextInt();
+        }
+
+        if (turk<0 || turk>= 101){
+            System.out.println("Türkçe notu için 0-100 arasında bir not değeri giriniz.");
+        }
+
+        else if (turk>0 && turk< 101) {
+            System.out.print("Kimya Ders Notunu Girin : ");
+            kim = input.nextInt();
+
+        }
+        if (kim<0 || kim>= 101) {
+            System.out.println("Kimya notu için 0-100 arasında bir not değeri giriniz.");
+        }
+        else if (kim>0 && kim< 101){
+            System.out.print("Müzik Ders Notunu Girin : ");
+            muz = input.nextInt();
+        }
+        if  (muz<0 || muz>= 101){
+            System.out.println("Müzik notu için 0-100 arasında bir not değeri giriniz.");
+        }
+        else if(muz>0 && muz< 101)  {
+            ortalama = ((mat + fiz + turk + kim + muz) / 5);
+            if (ortalama >= 55) {
+                System.out.println(("Geçtiniz: ")+ (ortalama));
+            } else {
+                System.out.println(("Kaldınız: ")+ (ortalama));
+            }
+
+
+        }
+
+    }
+}
